165c64a899afea75ee6196d645ab765c20456218
jeremiahmaxwellang <1jma7fltcw@gmail.com>
2026-02-04 14:08:40 +0800

    database: insert users script

---
+++ database/insert.sql
@@ -0,0 +1,27 @@
+USE teamforgedb;
+
+INSERT INTO users(userId, email, passwordHash, firstname, lastname, position, discord, status) 
+VALUES 
+(
+    1,
+    'jeremiah_ang@dlsu.edu.ph',
+    '1234',
+    'Jeremiah',
+    'Ang',
+    'Team Manager',
+    'Cowwrean#cowwrean',
+    'Active'
+);
+
+INSERT INTO users(userId, email, passwordHash, firstname, lastname, position, discord, status)
+VALUES 
+(
+    2,
+    'trebis_detablan@dlsu.edu.ph',
+    '1234',
+    'Trebis',
+    'Detablan',
+    'Player',
+    'hailrain#hailrain',
+    'Active'
+);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
+const mySqlPool = require('../config/database');
 
-// Example on how to use Controllers
-const {getContact} = require("../controllers/contactController");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
-
 app.set("view engine", 'hbs');
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Example on how to use Routes
+// Routes 
 // https://www.youtube.com/watch?v=H9M02of22z4
+app.use('/api/v1/users', require("../routes/userRoutes"));
 app.use("/api/contacts", require("../routes/contactRoutes"));
 
-// Login page = Landing Page
-// Route to login.html
-// localhost:3000
+// Controllers
+const {getUsers} = require("../controllers/userController");
+const {getContact} = require("../controllers/contactController");
+
+
+// Login
 app.get('/', async function(req, res) {
     res.sendFile(path.join(__dirname, '../views/login.html')); 
     //change this back to login.html
@@ -41,6 +42,10 @@ app.get('/player_analysis', async function(req, res) {
     res.sendFile(path.join(__dirname, '../views/player_analysis.html')); 
 });
 
+
+mySqlPool.query('SELECT 1').then(() => {
+    console.log('MySQL DB Connected');
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
