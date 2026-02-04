b3be3e33bc03848567dafc1c332aa346e97b35ff
jeremiahmaxwellang <1jma7fltcw@gmail.com>
2026-02-04 17:10:36 +0800

    routes: create and delete users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const { getUsers } = require('../controllers/userController');
+const { 
+    getUsers, 
+    getUserById, 
+    createUser,
+    deleteUser
+} = require('../controllers/userController');
 
 const router = express.Router();
 
 // Routes
 router.get('/getall', getUsers); // http://localhost:3000/api/v1/users/getall
+
+router.get('/get/:id', getUserById); // http://localhost:3000/api/v1/users/get/2
+
+router.post('/create', createUser); // http://localhost:3000/api/v1/users/create
+
+router.delete('/delete/:id', deleteUser); // http://localhost:3000/api/v1/users/delete/2
 
 module.exports = router;
