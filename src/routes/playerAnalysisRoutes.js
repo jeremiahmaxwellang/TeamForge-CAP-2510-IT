21573ce6961b13a8f237f5267f68f2e59baf01b7
Duelly01 <charles_duelas@dlsu.edu.ph>
2026-02-06 17:54:54 +0800

    NEW FEATURE: there are overlays for all the tabs section

--- src/routes/playerAnalysisRoutes.js
+++ src/routes/playerAnalysisRoutes.js
@@ -26,4 +26,24 @@ router.get('/overview', async function(req, res) {
     res.sendFile(path.join(viewsPath, 'player_analysis_overlays/player_overview.html'));
 });
 
+// Serve overlay HTML for player comparison
+router.get('/comparison', async function(req, res) {
+    res.sendFile(path.join(viewsPath, 'player_analysis_overlays/player_comparison.html'));
+});
+
+// Serve overlay HTML for player VODs
+router.get('/vods', async function(req, res) {
+    res.sendFile(path.join(viewsPath, 'player_analysis_overlays/player_vod.html'));
+});
+
+// Serve overlay HTML for player champion pool
+router.get('/champion', async function(req, res) {
+    res.sendFile(path.join(viewsPath, 'player_analysis_overlays/player_champion_pool.html'));
+});
+
+// Serve overlay HTML for player evaluation
+router.get('/evaluation', async function(req, res) {
+    res.sendFile(path.join(viewsPath, 'player_analysis_overlays/player_evaluation.html'));
+});
+
 module.exports = router;
