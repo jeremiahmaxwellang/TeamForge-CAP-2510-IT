ed23b0f76d5a7bb970bf78f32a91c9c11b2cd810
jeremiahmaxwellang <165755094+jeremiahmaxwellang@users.noreply.github.com>
2026-02-06 15:26:25 +0800

    config: changed database credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+
 let mysql = require('mysql2/promise');
 
 const mySqlPool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'root1234!',
-    database:'teamforgedb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-// password may vary from device to device
+// password stored in /src/.env
 
 module.exports = mySqlPool;
