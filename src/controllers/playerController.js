27b3b5bb8e396a88eaa20a7cb9b47a9cac8672cf
jeremiahmaxwellang <165755094+jeremiahmaxwellang@users.noreply.github.com>
2026-02-07 12:29:19 +0800

    controller: updated sql queries for new schema

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -4,10 +4,14 @@ const db = require("../config/database");
 exports.getAllPlayers = async (req, res) => {
   try {
     const sql = `
-      SELECT u.*, p.*
+	  SELECT u.*, p.*, 
+      r1.displayedRole AS primaryRole, r2.displayedRole AS secondaryRole
       FROM users u
       JOIN players p ON u.userId = p.userId
+	    JOIN leagueRoles r1 ON p.primaryRoleId = r1.roleId
+      JOIN leagueRoles r2 ON p.secondaryRoleId = r2.roleId
       WHERE u.position = 'Player'
+      ORDER BY primaryRoleId;
     `;
     const [results] = await db.query(sql);
     res.json(results);
@@ -21,10 +25,14 @@ exports.getPlayerById = async (req, res) => {
   try {
     const playerId = req.params.id;
     const sql = `
-      SELECT u.*, p.*
+      SELECT u.*, p.*, 
+      r1.displayedRole AS primaryRole, r1.role AS riotRole1, r1.teamPosition AS riotTeamPosition1,
+      r2.displayedRole AS secondaryRole, r2.role AS riotRole2, r2.teamPosition AS riotTeamPosition2
       FROM users u
       JOIN players p ON u.userId = p.userId
-      WHERE u.userId = ? AND u.position = 'Player'
+      JOIN leagueRoles r1 ON p.primaryRoleId = r1.roleId
+      JOIN leagueRoles r2 ON p.secondaryRoleId = r2.roleId
+      WHERE u.userId = ? AND u.position = 'Player';
     `;
     const [results] = await db.query(sql, [playerId]);
     if (results.length === 0) {
