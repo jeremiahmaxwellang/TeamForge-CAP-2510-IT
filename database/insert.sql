0321dea28dbec3eedf4bed716e3b9a9875dd8c9b
jeremiahmaxwellang <165755094+jeremiahmaxwellang@users.noreply.github.com>
2026-02-17 17:30:54 +0800

    database: added tables for storing players' average statistics
    TODO:
    - Run the new teamforgedb.sql script
    - Run the new inserts.sql script

--- database/insert.sql
+++ database/insert.sql
@@ -11,12 +11,7 @@ VALUES
 (2, 'Jungle', 'NONE', 'JUNGLE'),
 (3, 'Mid', 'SOLO', 'MIDDLE'),
 (4, 'AD Carry', 'CARRY', 'BOTTOM'),
-(5, 'Support', 'SUPPORT', 'UTILITY'),
-(6, 'Sub Top ', 'NONE', 'TOP'),
-(7, 'Sub Jungle', 'NONE', 'JUNGLE'),
-(8, 'Sub Mid', 'SOLO', 'MIDDLE'),
-(9, 'Sub AD Carry', 'CARRY', 'BOTTOM'),
-(10, 'Sub Support', 'SUPPORT', 'UTILITY');
+(5, 'Support', 'SUPPORT', 'UTILITY');
 
 -- Manager
 INSERT INTO users(userId, email, passwordHash, firstname, lastname, position, discord, status) 
@@ -120,7 +115,7 @@ VALUES
     'Active'
 );
 
-INSERT INTO players(userId, gameName, tagLine, currentRank, peakRank, primaryRoleId, secondaryRoleId, schoolId, course, lastGPA, CGPA, yearLevel)
+INSERT INTO players(userId, gameName, tagLine, currentRank, peakRank, primaryRoleId, secondaryRoleId, schoolId, course, lastGPA, CGPA, yearLevel, isSub)
 VALUES 
 (
     3,
@@ -134,7 +129,8 @@ VALUES
     'BSCS-SE',
     '3.4',
     '3.3',
-    '3rd Year'
+    '3rd Year',
+    'F'
 ),
 (
     4,
@@ -148,7 +144,8 @@ VALUES
     'BSCS-NIS',
     '3.5',
     '3.2',
-    '2nd Year'
+    '2nd Year',
+    'F'
 ),
 (
     5,
@@ -162,7 +159,8 @@ VALUES
     'BSIT',
     '3.1',
     '2.9',
-    '4th Year'
+    '4th Year',
+    'F'
 ),
 (
     6,
@@ -176,7 +174,8 @@ VALUES
     'BSND',
     '3.5',
     '3.2',
-    '3rd Year'
+    '3rd Year',
+    'F'
 ),
 (
     7,
@@ -190,7 +189,8 @@ VALUES
     'BSIT',
     '2.9',
     '3.0',
-    '4th Year'
+    '4th Year',
+    'F'
 ),
 (
     8,
@@ -198,13 +198,14 @@ VALUES
     'Miko',
     'Diamond II',
     'Diamond IV',
-    7,
+    2,
     1,
     '12404219',
     'BSIT',
     '3.5',
     '3.1',
-    '2nd Year'
+    '2nd Year',
+    'T'
 ),
 (
     9,
@@ -218,7 +219,8 @@ VALUES
     'BSIT',
     '2.1',
     '1.9',
-    '2nd Year'
+    '2nd Year',
+    'F'
 );
 
 -- Applicants
@@ -297,3 +299,161 @@ VALUES (
     2.9,
     'Pending'
 );
+
+-- -----------------------------------------------------
+-- METRICS
+-- -----------------------------------------------------
+INSERT INTO metrics (metricId, metricName, metricDescription) VALUES
+(1,  'averageAdcProximityAt15', 'Average Proximity Time to ADC by 15 minutes'),
+(2,  'averageAssists', 'Average Assists per game'),
+(3,  'averageCsDiffAt15', 'Average CS Difference at 15 minutes'),
+(4,  'averageCsPerMinute', 'Average Creep Score per Minute'),
+(5,  'averageDamageShare', 'Average Damage Share Percentage'),
+(6,  'averageDamageToBuildings', 'Average Damage dealt to buildings'),
+(7,  'averageDeaths', 'Average Deaths per game'),
+(8,  'averageDragonKills', 'Average Dragon Kills'),
+(9,  'averageEnemyJungleControl', 'Average Enemy Jungle Monster Kills'),
+(10, 'averageGoldDiffAt15', 'Average Gold Difference at 15 minutes'),
+(11, 'averageGoldPerMinute', 'Average Gold Per Minute'),
+(12, 'averageKDA', 'Average Kill/Death/Assist Ratio'),
+(13, 'averageKillParticipation', 'Average Kill Participation Percentage'),
+(14, 'averageKills', 'Average Kills per game'),
+(15, 'averageSoloKills', 'Average Solo Kills per game'),
+(16, 'averageTeamBaronKills', 'Average Team Baron Kills'),
+(17, 'averageTeamElderDragonKills', 'Average Team Elder Dragon Kills'),
+(18, 'averageTeamRiftHeraldKills', 'Average Team Rift Herald Kills'),
+(19, 'averageTotalDamageTaken', 'Average Total Damage Taken'),
+(20, 'averageVisionScorePerMinute', 'Average Vision Score Per Minute'),
+(21, 'averageVisionScoreShare', 'Average Vision Score Share Percentage'),
+(22, 'averageVoidMonsterKills', 'Average Void Monster Kills'),
+(23, 'averageWardsDestroyed', 'Average Total Wards Destroyed'),
+(24, 'averageWardsPlaced', 'Average Total Wards Placed'),
+(25, 'averageXpDiffAt15', 'Average Experience Difference at 15 minutes'),
+(26, 'averageWinrate', 'Average Winrate');
+
+-- -----------------------------------------------------
+-- METRIC ROLES
+-- -----------------------------------------------------
+
+-- 1. Top
+INSERT INTO metricRoles (metricId, roleId) VALUES
+(26,1), -- averageWinrate
+(4,1),  -- averageCsPerMinute
+(11,1), -- averageGoldPerMinute
+(3,1),  -- averageCsDiffAt15
+(25,1), -- averageXpDiffAt15
+(7,1),  -- averageDeaths
+(15,1), -- averageSoloKills
+(13,1), -- averageKillParticipation
+(6,1),  -- averageDamageToBuildings
+(19,1); -- averageTotalDamageTaken
+
+-- 2. Jungle
+INSERT INTO metricRoles (metricId, roleId) VALUES
+(26,2),  -- averageWinrate
+(4,2),   -- averageCsPerMinute
+(13,2),  -- averageKillParticipation
+(20,2),  -- averageVisionScorePerMinute
+(3,2),   -- averageCsDiffAt15
+(8,2),   -- averageDragonKills
+(16,2),  -- averageTeamBaronKills
+(17,2),  -- averageTeamElderDragonKills
+(18,2),  -- averageTeamRiftHeraldKills
+(22,2),  -- averageVoidMonsterKills
+(9,2);   -- averageEnemyJungleControl
+
+-- 3. Mid
+INSERT INTO metricRoles (metricId, roleId) VALUES
+(26,3),  -- averageWinrate
+(4,3),   -- averageCsPerMinute
+(11,3),  -- averageGoldPerMinute
+(13,3),  -- averageKillParticipation
+(14,3),  -- averageKills
+(7,3),   -- averageDeaths
+(2,3),   -- averageAssists
+(5,3);   -- averageDamageShare
+
+-- 4. ADC
+INSERT INTO metricRoles (metricId, roleId) VALUES
+(26,4),  -- averageWinrate
+(12,4),  -- averageKDA
+(20,4),  -- averageVisionScorePerMinute
+(4,4),   -- averageCsPerMinute
+(3,4),   -- averageCsDiffAt15
+(25,4),  -- averageXpDiffAt15
+(10,4),  -- averageGoldDiffAt15
+(11,4),  -- averageGoldPerMinute
+(5,4);   -- averageDamageShare
+
+-- 5. Support
+INSERT INTO metricRoles (metricId, roleId) VALUES
+(26,5),  -- averageWinrate
+(21,5),  -- averageVisionScoreShare
+(20,5),  -- averageVisionScorePerMinute
+(7,5),   -- averageDeaths
+(2,5),   -- averageAssists
+(13,5),  -- averageKillParticipation
+(24,5),  -- averageWardsPlaced
+(23,5),  -- averageWardsDestroyed
+(1,5);   -- averageAdcProximityAt15
+
+-- -----------------------------------------------------
+-- BENCHMARKS
+-- -----------------------------------------------------
+
+-- 1. Top
+INSERT INTO benchmarks (metricId, roleId, benchmarkValue, comparator) VALUES
+(4, 1, 7.80, '>='),
+(11, 1, 388.40, '>='),
+(3, 1, -0.30, '>='),
+(25, 1, -31.40, '>='),
+(7, 1, 3.20, '<='),
+(15, 1, 1.70, '>='),
+(13, 1, 0.44, '>='), -- Kill Participation
+(6, 1, 0.21, '>='),  -- Damage to Buildings
+(19, 1, 0.25, '>='); -- Total Damage Taken
+
+-- 2. Jungle
+INSERT INTO benchmarks (metricId, roleId, benchmarkValue, comparator) VALUES
+(4, 2, 6.80, '>='),
+(13, 2, 0.67, '>='), -- Kill Participation
+(20, 2, 1.60, '>='),
+(3, 2, 0.00, '>'),
+(8, 2, 2.00, '>='),
+(16, 2, 1.00, '>='),
+(17, 2, 1.00, '>='),
+(18, 2, 1.00, '>='),
+(22, 2, 2.00, '>='),
+(9, 2, 4.00, '>=');
+
+-- 3. Mid
+INSERT INTO benchmarks (metricId, roleId, benchmarkValue, comparator) VALUES
+(4, 3, 8.60, '>='),
+(11, 3, 400.00, '>='), -- Gold Per Minute (GPM)
+(13, 3, 0.62, '>='),
+(14, 3, 4.00, '>='),
+(7, 3, 3.20, '<='),
+(2, 3, 6.60, '>='),
+(5, 3, 0.25, '>='); -- Damage Share
+
+-- 4. ADC
+INSERT INTO benchmarks (metricId, roleId, benchmarkValue, comparator) VALUES
+(12, 4, 4.10, '>='),
+(20, 4, 1.00, '>='),
+(4, 4, 8.90, '>='),
+(3, 4, -0.25, '>='),
+(25, 4, -6.75, '>='),
+(10, 4, 7.75, '>='),
+(11, 4, 460.00, '>='),
+(5, 4, 0.25, '>='); -- Damage Share
+
+-- 5. Support
+INSERT INTO benchmarks (metricId, roleId, benchmarkValue, comparator) VALUES
+(21, 5, 0.43, '>='), -- Vision Score Share
+(20, 5, 3.60, '>='),
+(7, 5, 4.00, '<='),
+(2, 5, 12.60, '>='),
+(13, 5, 0.76, '>='), -- Kill Participation
+(24, 5, 5.00, '>='),
+(23, 5, 5.00, '>='),
+(1, 5, 0.55, '>='); -- Proximity Time to ADC by 15 Minutes
