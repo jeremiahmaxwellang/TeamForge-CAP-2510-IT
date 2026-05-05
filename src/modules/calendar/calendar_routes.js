6842d4071eaee221dab9d8b14ed6ddaf2657798f
jeremiahmaxwellang <165755094+jeremiahmaxwellang@users.noreply.github.com>
2026-05-05 12:34:31 +0800

    [DOCS]: noted that calendar controller hasn't been connected to index.js yet

--- src/modules/calendar/calendar_routes.js
+++ src/modules/calendar/calendar_routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const calendarController = require('./calendar_controller');
 
+// Note: I haven't connected these routes to index.js yet
+
 // Route to load the webpage
 router.get('/', calendarController.getCalendarPage);
 
