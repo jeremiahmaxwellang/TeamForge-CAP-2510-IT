fc71a290f17effb145c5beab06e933eaadde141b
jeremiahmaxwellang <165755094+jeremiahmaxwellang@users.noreply.github.com>
2026-06-17 16:38:27 +0800

    [ANNOUNCEMENTS]: Fixed bug of announcements not displaying

--- src/modules/calendar/calendar_controller.js
+++ src/modules/calendar/calendar_controller.js
@@ -22,8 +22,8 @@ exports.getAvailability = async (req, res) => {
                    r1.displayedRole as primaryRole, r2.displayedRole as secondaryRole
             FROM users u
             JOIN players p ON u.userId = p.userId
-            LEFT JOIN leagueRoles r1 ON p.primaryRoleId = r1.roleId
-            LEFT JOIN leagueRoles r2 ON p.secondaryRoleId = r2.roleId
+            LEFT JOIN leagueroles r1 ON p.primaryRoleId = r1.roleId
+            LEFT JOIN leagueroles r2 ON p.secondaryRoleId = r2.roleId
             WHERE u.position IN ('Player', 'Sub') AND u.status = 'Active'
         `);
 
